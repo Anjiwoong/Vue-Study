2ed51af4016cbbdf23ca2510533ba767d4884f92
Woong <rlqmatk@naver.com>
2023-03-02 17:07:15 +0900

    Feat: Section09/126

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,37 +1,41 @@
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
-    <UserInfo
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    />
-    <course-goals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps['another-prop'] }}</p>
-      </template>
-    </course-goals>
+    <button @click="setSelectedComponents('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponents('manage-goals')">Manage Goals</button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'active-goals'" />
+    <ManageGoals v-if="selectedComponent === 'manage-goals'" /> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
-import BadgeList from './components/BadgeList.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import TheHeader from './components/layout/TheHeader.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
-  components: { TheHeader, UserInfo, BadgeList, CourseGoals },
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+  methods: {
+    setSelectedComponents(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
